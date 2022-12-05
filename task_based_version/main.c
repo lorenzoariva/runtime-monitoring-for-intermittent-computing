6d540d6529dfde25ae5936aa0d4271c332ca3c5e
LorenzoUniTN <lorenzoantonio.riva@studenti.unitn.it>
2022-12-05 14:17:14 +0100

    github repo restored

--- task_based_version/main.c
+++
@@ -1,9 +0,0 @@
-#include <msp430.h>
-#include "tiny-ink/ink.h"
-
-int main(void) {
-    /* start ink system */
-    ink_boot();
-
-    return 0;
-}
--- task_based_version/tiny-ink/mcu/mcu.h
+++
@@ -1,7 +0,0 @@
-#ifndef MCU_H_
-#define MCU_H_
-
-#include "msp430/fram.h"
-#include "msp430/msp430fr5969.h"
-
-#endif /* MCU_H_ */
